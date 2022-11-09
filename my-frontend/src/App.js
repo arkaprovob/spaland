83032b7d956730280e7a3ce6206ce1240af861f4
Arko <47105136+arkaprovob@users.noreply.github.com>
2022-11-09 08:07:41 +0530

    fix(my-fe): deployment Error fix

--- my-frontend/src/App.js
+++ my-frontend/src/App.js
@@ -1,11 +1,11 @@
-import logo from 'https://avatars.githubusercontent.com/u/47105136?v=4';
+//import logo from 'https://avatars.githubusercontent.com/u/47105136?v=4';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src='https://avatars.githubusercontent.com/u/47105136?v=4' className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
